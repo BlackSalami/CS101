0fcf782853138bacb9a76a9a31817e9b87a8142f
BlackSalami <17136725+BlackSalami@users.noreply.github.com>
2021-01-11 02:58:32 -0700

    Prep for actual usage

---
+++ CS101/1 - Hello World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1___Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Class, this is my example of a Hello World Console App in C#!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CS101/Blank Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blank_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
