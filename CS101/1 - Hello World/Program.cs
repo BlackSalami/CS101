8658f8a47e87c2f0a7d893b7ea894e6bd02103aa
BlackSalami <17136725+BlackSalami@users.noreply.github.com>
2021-01-11 04:24:10 -0700

    Final Commit for Lesson 1

--- CS101/1 - Hello World/Program.cs
+++ CS101/1 - Hello World/Program.cs
@@ -7,7 +7,16 @@ namespace _1___Hello_World
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Class, this is my example of a Hello World Console App in C#!");
+
             Console.ReadKey();
+
+            //this is a single line comment
+
+            /*
+             * this is a 
+             * multi-line
+             * comment block.
+             */
         }
     }
 }
